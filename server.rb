b86c321bbbb091c15c17fd6467573b4884b18654
kyoro <kyoro@hakamastyle.net>
2013-05-26 18:02:31 +0900

    bootstrap integrated

--- server.rb
+++ server.rb
@@ -3,11 +3,11 @@ get '/' do
 end
 
 get '/on' do
-  `gpio -g write 25 1`
+  system("gpio -g write 25 1")
   erb :on
 end
 
 get '/off' do
-  `gpio -g write 25 0`
+  system("gpio -g write 25 0")
   erb :off
 end
